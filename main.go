d4bb56dc5f8105e48db66df061ce9f79ee021ca3
holynnchen <holynnchen@tencent.com>
2021-03-23 12:45:23 +0800

    修改参数

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	center := DanmuCenter.NewDanmuCenter(&DanmuCenter.DanmuCenterConfig{
 		TimeRange:      16,
-		MonitorNumber:  150,
+		MonitorNumber:  200,
 		SpecialFocusOn: []int{1370218}, //1237390
 		// Silent:         true,
 	},
