12456713778d58eb1d12a7d6b98d977b92b6b995
holynnchen <holynnchen@tencent.com>
2021-04-10 02:10:33 +0800

    恢复数量

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 
 	center := DanmuCenter.NewDanmuCenter(&DanmuCenter.DanmuCenterConfig{
 		TimeRange:      16,
-		MonitorNumber:  100,
+		MonitorNumber:  500,
 		SpecialFocusOn: []int{1370218}, //1237390
 		Silent:         true,
 	},
