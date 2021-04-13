c962d192b77ff7ddac8752d3f0d19a1300ecd845
holynnchen <holynnchen@tencent.com>
2021-04-13 21:18:06 +0800

    remove log

--- DanmuCenter/Filter/highSimilarityAndSpeedFilter.go
+++ DanmuCenter/Filter/highSimilarityAndSpeedFilter.go
@@ -1,8 +1,6 @@
 package Filter
 
 import (
-	"log"
-
 	"github.com/Holynnchen/BiliBan2/DanmuCenter"
 	"github.com/Holynnchen/BiliBan2/DanmuCenter/Utils"
 )
@@ -23,10 +21,6 @@ func (filter *HighSimilarityAndSpeedFilter) Check(center *DanmuCenter.DanmuCente
 	for i := 1; i < dataLen; i++ {
 		allCompare = (allCompare*float32(i-1) + Utils.GetSimilarity(danmuList[dataLen-i].Content, danmuList[dataLen-i-1].Content)) / float32(i)
 		if i > filter.startCheck-1 && allCompare > filter.similarity {
-			for _,danmu := range danmuList{
-				log.Printf("%+v ",*danmu)
-			}
-			log.Println()
 			return true, "时间范围内近似发言过多"
 		}
 	}
