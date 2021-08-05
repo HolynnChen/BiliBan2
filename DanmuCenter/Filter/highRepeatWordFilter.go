ad1ea5fca55022dd5efc5403095e994ade4b20e1
holynnchen <holynnchen@tencent.com>
2021-08-05 09:24:32 +0000

    update

--- DanmuCenter/Filter/highRepeatWordFilter.go
+++ DanmuCenter/Filter/highRepeatWordFilter.go
@@ -1,8 +1,6 @@
 package Filter
 
 import (
-	"unicode/utf8"
-
 	"github.com/Holynnchen/BiliBan2/DanmuCenter"
 )
 
@@ -14,13 +12,15 @@ type HighRepeatWordFilter struct {
 func (filter *HighRepeatWordFilter) Check(center *DanmuCenter.DanmuCenter, danmu *DanmuCenter.Danmu) (bool, string) {
 	countMap := make(map[rune]int)
 	max := 0
+	all := 0
 	for _, data := range danmu.Content {
 		countMap[data]++
+		all++
 		if countMap[data] > max {
 			max = countMap[data]
 		}
 	}
-	return float32(max)/float32(utf8.RuneCountInString(danmu.Content)) > filter.repeatTarget, ""
+	return float32(max)/float32(all) > filter.repeatTarget, ""
 }
 
 func NewHighReatWordFilter(repeatTarget float32) *HighRepeatWordFilter {
